436f3c292422f9e010ca00c7205ba68538360fb7
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-01-02 18:12:55 +0100

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Lista Zadań';
+
+  get footer(): string {
+    return '2020 © Lista zadań,All rights reserved.';
+  }
+}
