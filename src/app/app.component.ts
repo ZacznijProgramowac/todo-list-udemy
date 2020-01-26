2b294dca9ada3d36b06d9debdd78cde14c3ddc5e
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-01-26 16:18:44 +0100

    Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Task } from './task';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  taskName: string;
   config: { [key: string]: string | Date } = null;
   tasks: Task[] = [
     {
@@ -34,5 +35,23 @@ export class AppComponent {
         date: new Date(),
       };
     }, 500);
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    const target = event.target as HTMLInputElement;
+    this.taskName = target.value;
+  }
+
+  createTask() {
+    const task: Task = {
+      name: this.taskName,
+      deadline: '2020-02-02',
+      done: false,
+    };
+    this.tasks.push(task);
   }
 }
