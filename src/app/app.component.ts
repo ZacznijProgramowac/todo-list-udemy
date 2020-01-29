ce10a04b624c872623ffde9d5cb59e11959e9463
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-01-29 20:02:07 +0100

    Template reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Task } from './task';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  taskName: string;
   config: { [key: string]: string | Date } = null;
   tasks: Task[] = [
     {
@@ -41,15 +40,10 @@ export class AppComponent {
     this.tasks = [];
   }
 
-  onKeyUp(event: KeyboardEvent) {
-    const target = event.target as HTMLInputElement;
-    this.taskName = target.value;
-  }
-
-  createTask() {
+  createTask(name: string, deadline: string) {
     const task: Task = {
-      name: this.taskName,
-      deadline: '2020-02-02',
+      name,
+      deadline,
       done: false,
     };
     this.tasks.push(task);
