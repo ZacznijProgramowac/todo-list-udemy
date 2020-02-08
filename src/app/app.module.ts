2c1661596766384fb1aa175a43a0a5197ba6d71f
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-02-08 16:34:15 +0100

    Two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import localePl from '@angular/common/locales/pl';
 import { registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 registerLocaleData(localePl);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
