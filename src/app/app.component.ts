cad6fc557b045050ac4c4bf804712b7ed600c4ad
Spinomik <spinomik@onet.pl>
2022-10-01 14:14:53 +0200

    7 -Interpolacja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Lista zadań';
+
+  get footer(): string {
+    return '2022 © Lista zadań,All rights reserved.';
+  }
+}
