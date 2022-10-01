813e51e9f8d4ecde80adc686d850fe80c219bb87
Spinomik <spinomik@onet.pl>
2022-10-01 15:57:37 +0200

    10-NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { formatDate } from '@angular/common';
+import { Task } from './tasks';
 
 
 @Component({
@@ -9,15 +10,29 @@ import { formatDate } from '@angular/common';
 })
 export class AppComponent {
   config: { [key: string]: string } | null = null;
+  tasks: Task[] = [
+    {
+      name: 'Wyrzucić śmieci',
+      deadline: '2022-10-02',
+      done: false
+    }, {
+      name: 'Nakarmić kota',
+      deadline: '2022-10-01',
+      done: false
+    }, {
+      name: 'Zrobić zakupy',
+      deadline: '2022-10-03',
+      done: false
+    }
+  ];
 
-  constructor(){
-    setTimeout(() => {
-      this.config ={
-        title: 'Lista zadań',
-        footer: 'Lista zadań zbudowana w Angularze',
-        date: formatDate(new Date(),'yyyy','pl-PL')
-      }
-    },1000)
-    
+  constructor() {
+
+    this.config = {
+      title: 'Lista zadań',
+      footer: 'Lista zadań zbudowana w Angularze',
+      date: formatDate(new Date(), 'yyyy', 'pl-PL')
+    }
+
   }
 }
