f0ab78a35c66c19b33a2c2b464195335426c7ec5
Spinomik <spinomik@onet.pl>
2022-10-04 19:06:54 +0200

    Aplication Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,20 +26,20 @@ export class AppComponent {
       name: 'Zrobić zakupy',
       deadline: '2022-10-03',
       done: false
-    },{
+    }, {
       name: 'Zrobić obiad',
       deadline: '2022-10-03',
-      done: true
+      done: false
     }
   ];
 
   constructor() {
-
     this.config = {
       title: 'Lista zadań',
       footer: 'Lista zadań zbudowana w Angularze',
       date: formatDate(new Date(), 'yyyy', 'pl-PL')
     }
+    this.sortTask();
   }
 
   clearTasks(): void {
@@ -54,12 +54,31 @@ export class AppComponent {
       done: false
     }
     this.tasks.push(task);
-    this.taskName ='';
-    this.taskDate='';
+    this.taskName = '';
+    this.taskDate = '';
+    this.sortTask();
   }
 
-  changeButton(){
+  changeButton() {
     this.editMode = !this.editMode;
+  }
+
+  markTaskDone(task: Task) {
+    task.done = !task.done;
+    this.sortTask();
+  }
+
+  removeTask(task: Task) {
+    this.tasks = this.tasks.filter(
+      value => value !== task
+    )
+    this.sortTask();
+  }
+
+  private sortTask() {
+    this.tasks = this.tasks.sort((a: Task, b: Task) =>
+      a.done === b.done ? 0 : a.done ? 1 : -1
+    );
   }
 
 }
